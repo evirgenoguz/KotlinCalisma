d3e05c30564f48025c4b9cce0dea650c207b44b4
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-10-07 23:14:07 +0300

    lemonade uygulaması

--- ZarUygulamasi/app/src/main/java/com/evirgenoguz/zaruygulamasi/MainActivity.kt
+++ ZarUygulamasi/app/src/main/java/com/evirgenoguz/zaruygulamasi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.evirgenoguz.zaruygulamasi
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,33 +15,27 @@ class MainActivity : AppCompatActivity() {
 
         var rollButton: Button = findViewById(R.id.roll)
 
-        rollButton.setOnClickListener {
-
-            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_LONG)
-            //toast.show()
-
-            //val resultTextView: TextView = findViewById(R.id.textView)
-            //resultTextView.text = "6"
-
-            rollDice()
-
-
-        }
+        rollButton.setOnClickListener { rollDice() }
 
     }
 
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val diceRoll2 = dice.roll()
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
 
-        var sum: Int = diceRoll+diceRoll2
-        val sumTextView: TextView = findViewById(R.id.textView3)
-        sumTextView.text = sum.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceRoll = dice.roll()
+        diceImage.contentDescription = diceRoll.toString()
+
+        val drawebleResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawebleResource)
 
 
     }
