bcd15735b3d61fb02f7606b1c404e451514520b1
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-12-03 16:26:18 +0300

    Firebase auth

--- FotografFirebaseBTK/app/src/main/java/com/evirgenoguz/fotograffirebasebtk/HaberlerActivity.kt
+++ FotografFirebaseBTK/app/src/main/java/com/evirgenoguz/fotograffirebasebtk/HaberlerActivity.kt
@@ -1,16 +1,53 @@
 package com.evirgenoguz.fotograffirebasebtk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.evirgenoguz.fotograffirebasebtk.databinding.ActivityHaberlerBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HaberlerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHaberlerBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHaberlerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.secenekler_menusu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //hangi item seçildiyse ne ypaıldıgını bu methodun içinde yazıyoruz
+
+        if (item.itemId == R.id.fotograf_paylas){
+            //fotograf paylaşma aktivitesine gidilecek
+            val intent = Intent(this, FotografPaylasmaActivity::class.java)
+            startActivity(intent)
+        }
+        else if(item.itemId == R.id.cikis_yap){
+            //firebaseden de çıkış yapmamız gerekiyor.
+            auth.signOut() //çıkış her türlü yapılır. Listener a gerek yok
+
+            val intent = Intent(this, KullaniciAcitivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
--- FotografFirebaseBTK/app/src/main/java/com/evirgenoguz/fotograffirebasebtk/KullaniciAcitivity.kt
+++ FotografFirebaseBTK/app/src/main/java/com/evirgenoguz/fotograffirebasebtk/KullaniciAcitivity.kt
@@ -1,28 +1,80 @@
 package com.evirgenoguz.fotograffirebasebtk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.evirgenoguz.fotograffirebasebtk.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class KullaniciAcitivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
+
+        val guncelKullanici = auth.currentUser
+        if (guncelKullanici != null){
+            val intent = Intent(this@KullaniciAcitivity, HaberlerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
         binding.apply {
-            buttonGirisYap.setOnClickListener{
-                
+
+            buttonGirisYap.setOnClickListener {
+                auth.signInWithEmailAndPassword(
+                    editTextEmailAddress.text.toString(),
+                    editTextPassword.text.toString()
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+
+                        val guncelKullanici = auth.currentUser?.email.toString()
+                        val intent = Intent(this@KullaniciAcitivity, HaberlerActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(
+                        this@KullaniciAcitivity,
+                        exception.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
+
 
             buttonKayTOl.setOnClickListener {
 
+                val email = editTextEmailAddress.text.toString()
+                val sifre = editTextPassword.text.toString()
+
+                auth.createUserWithEmailAndPassword(email, sifre).addOnCompleteListener { task ->
+                    //asenkron çalışıyor burası
+                    if (task.isSuccessful) {
+                        //diğer aktiviteye git
+                        val intent = Intent(this@KullaniciAcitivity, HaberlerActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(
+                        applicationContext,
+                        exception.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
-
         }
-
     }
+
 }
