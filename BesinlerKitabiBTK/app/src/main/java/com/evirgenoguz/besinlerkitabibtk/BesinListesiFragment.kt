2c55c68041cd02a05290558a051ef4f89cf1efd3
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-12-07 17:59:39 +0300

    Besinler Kitabı Baslangic

---
+++ BesinlerKitabiBTK/app/src/main/java/com/evirgenoguz/besinlerkitabibtk/BesinListesiFragment.kt
@@ -0,0 +1,29 @@
+package com.evirgenoguz.besinlerkitabibtk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.evirgenoguz.besinlerkitabibtk.databinding.FragmentBesinListesiBinding
+
+
+class BesinListesiFragment : Fragment() {
+
+    private lateinit var binding: FragmentBesinListesiBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentBesinListesiBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
